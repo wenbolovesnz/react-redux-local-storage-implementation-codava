38171bf7561b67aa59f06e7782f48c3c62843b80
Wenbo L <wenbolovesnz@gmail.com>
2016-09-14 16:46:13 +1200

    added react router

--- www/reactComponents/Root.js
+++ www/reactComponents/Root.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { Provider } from 'react-redux';
+import { Router, Route, browserHistory } from 'react-router';
 import App from './App';
 
-const Root = ({store}) => {
+const Root = ({ store }) => {
 	return (
-		<Provider store={store} >
-			<App />
+		<Provider store={ store } >
+			<Router history={ browserHistory }>
+				<Route path="*" component={ App }/>
+			</Router>
 		</Provider>
 	)
+};
+
+Root.propTypes = {
+	store: PropTypes.object.isRequired
 };
 
 export default Root;
